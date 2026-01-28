e10d612428e9d3bdced4dffc3e7ef1643e66db10
Suuli <suulitoo@hotmail.com>
2026-01-28 13:57:15 +0100

    debug: add heartbeat to server

--- server.js
+++ server.js
@@ -4,13 +4,20 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
+  console.log("Requête reçue pour /");
   res.send('Backend is alive!');
 });
 
 app.get('/auth/discord', (req, res) => {
+  console.log("Requête reçue pour /auth/discord");
   res.send('Auth endpoint is alive!');
 });
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Serveur de test démarré et à l'écoute sur le port ${PORT}`);
 });
+
+// Ce timer va nous dire si le serveur est encore vivant après 10 secondes
+setTimeout(() => {
+  console.log("Le serveur est toujours en vie après 10 secondes. C'est une bonne signe !");
+}, 10000);
